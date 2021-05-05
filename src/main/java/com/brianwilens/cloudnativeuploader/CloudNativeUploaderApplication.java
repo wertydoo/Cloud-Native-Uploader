8b7ea269c7d960ef515d7c9785f00c994eeefa50
Brian Wilens <brian.wilens@gmail.com>
2021-05-05 10:16:01 -0500

    initial commit of blank spring app configured to deploy on appengine

---
+++ src/main/java/com/brianwilens/cloudnativeuploader/CloudNativeUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.brianwilens.cloudnativeuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeUploaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNativeUploaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brianwilens/cloudnativeuploader/CloudNativeUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brianwilens.cloudnativeuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNativeUploaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
